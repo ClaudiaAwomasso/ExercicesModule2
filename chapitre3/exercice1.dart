3df86f16d6b408d75a14cd4ae21270c076cd5f1d
ClaudiaAwomasso <claudiaawomasso@gmail.com>
2025-01-06 11:49:21 +0100

    l'exercice du chapitre3

---
+++ chapitre3/exercice1.dart
@@ -0,0 +1,8 @@
+//Exercice 1 : Écrire un programme qui affiche tous les
+//nombres premiers entre 1 et 100.
+void main() {
+  int nombre = 0;
+  for (var i = 1; i < 100; i++) {
+    print(nombre += 1);
+  }
+}
---
+++ chapitre3/exercice2.dart
@@ -0,0 +1,10 @@
+void main() {
+  //Exercice 3 : Écrire une fonction qui calcule la moyenne
+//coefficiée des notes d’un élève de la Terminale.
+  int resultat = moyenneCoefficie(14, 3);
+  print(resultat);
+}
+
+int moyenneCoefficie(int note, int coefficient) {
+  return note * coefficient;
+}
